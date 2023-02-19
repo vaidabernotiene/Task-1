3b0d2b1278cfbb6ca161b5d852243fcab5b51515
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-02-19 16:01:26 +0200

    creating 3 mathematical functions

--- script.js
+++ script.js
@@ -16,5 +16,30 @@ formEl.addEventListener("submit", outputFunc);
 
 function outputFunc(e) {
   e.preventDefault();
-  
+
+  outputEl.innerHTML = "";
+  lb();
+  g();
+  oz();
+}
+
+function lb() {
+  let p1El = document.createElement("p");
+  p1El.id = "search";
+  p1El.textContent = `${inputEl.value} kg = ${inputEl.value * 2.2046} lb.`;
+  outputEl.append(p1El);
+}
+
+function g() {
+  let p2El = document.createElement("p");
+  p2El.id = "search";
+  p2El.textContent = `${inputEl.value} kg = ${inputEl.value / 0.001} g.`;
+  outputEl.append(p2El);
+}
+
+function oz() {
+  let p3El = document.createElement("p");
+  p3El.id = "search";
+  p3El.textContent = `${inputEl.value} kg = ${inputEl.value * 35.274} oz.`;
+  outputEl.append(p3El);
 }
